f8955a5c9bdafc23ae4dd6f01e5709fa721edeff
Nadya-Clariessa <nclriessa@gmail.com>
2024-09-18 10:48:54 +0700

    Update nadd.py

--- nadd.py
+++ nadd.py
@@ -6,7 +6,7 @@ import streamlit as st
 st.header('Nadya Clariessa')
 st.header('220322609234')
 st.subheader('Plot')
-st.latex(r'''\int_a^b\alpha^{\frac{1}{2}\omega x}frac{\partial^3x}{\partialx^3}dx''')
+st.latex(r'''\int_a^b\alpha^{\frac{1}{2}\omega x}frac{\partial^3x}{\partial x^3}dx''')
  
 x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)  # Generating x values from -2*pi to 2*pi
 y = np.sin(x)  # Calculating sin(x) values
